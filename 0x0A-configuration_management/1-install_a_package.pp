840fb00499654145e9ccbcad7b3c7124c6f5e86f
abdoomr2003 <abdoomer1112003@gmail.com>
2024-01-23 23:01:20 +0200

    A

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-exec { 'install-flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  unless  => '/usr/bin/pip3 freeze | grep Flask==2.1.0',
-  path    => ['/bin', '/usr/bin'],
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
-# create a manifest that kills a process named killmenow
+# kill process killmenow
 
-exec { 'kill-killmenow':
-  command => 'pkill -f killmenow',
-  onlyif  => 'pgrep -f killmenow',
-  path    => ['/bin', '/usr/bin'],
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }

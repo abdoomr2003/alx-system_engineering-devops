a9afdfab4a03d604d101cb4bf0b0ba2c7fc80a71
abdoomr2003 <abdoomer1112003@gmail.com>
2024-01-23 22:59:34 +0200

    A

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# create a manifest that kills a process named killmenow
+
+exec { 'kill-killmenow':
+  command => 'pkill -f killmenow',
+  onlyif  => 'pgrep -f killmenow',
+  path    => ['/bin', '/usr/bin'],
+}

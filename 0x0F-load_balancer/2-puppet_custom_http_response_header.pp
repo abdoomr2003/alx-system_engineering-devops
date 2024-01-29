1a70ce1bf9e846902419ea34395f5e95d9d0a799
abdoomr2003 <abdoomer1112003@gmail.com>
2024-01-30 00:42:26 +0200

    A

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,17 +1,49 @@
-# Add custom HTTP header  with Puppet
-exec { 'update':
-     command => '/usr/bin/apt-get update',
-}
+#!/usr/bin/env bash
+#  Add a custom HTTP header with Puppet
 
+# 2-puppet_custom_http_response_header.pp
+
+# Install Nginx package
 package { 'nginx':
-  ensure => installed,
+  ensure => present,
 }
 
-file { '/etc/nginx/conf.d/custom_header.conf':
+# Define a custom fact to get the hostname
+Facter.add('custom_hostname') do
+  setcode 'hostname'
+end
+
+# Configure Nginx with custom HTTP header
+file { '/etc/nginx/sites-available/default':
   ensure  => present,
-  content => 'add_header X-Served-By $http_header_value;',
+  content => "
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    server_name _;
+
+    location / {
+        add_header X-Served-By $custom_hostname;
+        # ... (other configurations)
+    }
+
+    # ... (other server blocks)
+}
+",
+  notify  => Service['nginx'],
 }
 
+# Enable the default site
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => link,
+  target  => '/etc/nginx/sites-available/default',
+  notify  => Service['nginx'],
+}
+
+# Restart Nginx to apply changes
 service { 'nginx':
-  ensure => running,
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
 }

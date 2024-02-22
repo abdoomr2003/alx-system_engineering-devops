2f85ce15a21cc2dbe9f88c239fbe1f8e384b8dd5
abdoomr2003 <abdoomer1112003@gmail.com>
2024-02-22 21:46:25 +0200

    A

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,7 +0,0 @@
-#  install flask from pip3
-
-exec { 'install-flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  unless  => '/usr/bin/pip3 freeze | grep Flask==2.1.0',
-  path    => ['/bin', '/usr/bin'],
-}

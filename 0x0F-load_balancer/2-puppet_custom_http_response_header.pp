6514a1f6aca11b15f8a727e2932a255950e3014b
abdoomr2003 <abdoomer1112003@gmail.com>
2024-03-04 22:01:47 +0200

    #  automate the task of creating a custom HTTP header response, but with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,18 @@
-# Define a custom fact to get the hostname
-Facter.add('nginx_hostname') do
-  setcode 'hostname'
-end
-
-# Install nginx package
+# 2-puppet_custom_http_response_header.pp
+#  automate the task of creating a custom HTTP header response, but with Puppet
+# Install Nginx package
 package { 'nginx':
   ensure => installed,
 }
 
-# Define custom header configuration file
+# Define custom HTTP header configuration file
 file { '/etc/nginx/conf.d/custom_headers.conf':
   ensure  => present,
-  content => "add_header X-Served-By ${::nginx_hostname};\n",
-  notify  => Service['nginx'],
+  content => "add_header X-Served-By $hostname;\n",
+  require => Package['nginx'],
 }
 
-# Restart nginx service
+# Restart Nginx service to apply changes
 service { 'nginx':
   ensure    => running,
   enable    => true,

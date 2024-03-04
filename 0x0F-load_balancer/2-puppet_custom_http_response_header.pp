e9be29b46c878eb0f1ca59a6eebb2786848d2bd5
abdoomr2003 <abdoomer1112003@gmail.com>
2024-03-04 22:05:12 +0200

    # automate the task of creating a custom HTTP header response, but with Puppet.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,25 +1,26 @@
 # 2-puppet_custom_http_response_header.pp
 # automate the task of creating a custom HTTP header response, but with Puppet.
-# Update package repositories
-package { 'nginx':
+# Install Nginx
+class { '::nginx':
   ensure => installed,
 }
 
-# Define a custom HTTP header file
-file { '/etc/nginx/sites-available/custom_header':
-  ensure  => file,
-  content => "add_header X-Served-By $hostname;",
-}
-
-# Create a symbolic link to the sites-enabled directory
-file { '/etc/nginx/sites-enabled/custom_header':
-  ensure => link,
-  target => '/etc/nginx/sites-available/custom_header',
-}
-
-# Restart Nginx to apply changes
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/custom_header'],
+# Define the custom HTTP header
+$server_name = $::hostname
+nginx::resource::server { 'default':
+  listen => ['80', '443 ssl'],
+  server_name => '_',
+  ssl => true,
+  ssl_certificate => '/etc/nginx/ssl/nginx.crt',
+  ssl_certificate_key => '/etc/nginx/ssl/nginx.key',
+  location => {
+    '/' => {
+      # Add the custom HTTP header
+      add_header => [
+        'X-Served-By "${server_name}"',
+      ],
+      # Serve a simple message
+      content => 'Hello world!',
+    },
+  },
 }

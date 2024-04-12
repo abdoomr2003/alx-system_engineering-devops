b32742698123015c6e63baefde7133df3816af6a
abdoomr2003 <abdoomer1112003@gmail.com>
2024-04-12 03:47:34 +0200

    A

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,19 @@
-# Fixes bad 'phpp'
-exec {
-    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
-    path => '/usr/local/bin/:/bin/'
+# 0-strace_is_your_friend.pp
+
+# Ensure Apache package is installed
+package { 'apache2':
+  ensure => installed,
+}
+
+# Ensure Apache service is running
+service { 'apache2':
+  ensure => running,
+}
+
+# Replace configuration file with fixed version
+file { '/etc/apache2/apache2.conf':
+  ensure  => file,
+  content => template('apache/apache2.conf.erb'),
+  require => Package['apache2'],
+  notify  => Service['apache2'],
 }
